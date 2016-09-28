96e5f14371990218a833d824757b989e9014f07a
Lopezsarmiento <diego.lopezsarmiento@gmail.com>
2016-09-28 17:23:13 -0300

    Adding Files
    Files Up to class 9 from Section 2.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*(1) Initializing angular - myApp shoud be added 
+in the HTML as ng-app = 'myApp'*/
+var myApp = angular.module('myApp', []);
+
+/*(2) Declaring a controller*/
+myApp.controller('mainController', function(){
+
+});
