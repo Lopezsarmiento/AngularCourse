5b87c20656fcd3c13715b7bf9c4bd655bbf1afbc
Lopezsarmiento <diego.lopezsarmiento@gmail.com>
2016-09-29 20:25:01 -0300

    End of Section 3
    Dependency injection and minification.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 /*(1) Initializing angular - myApp shoud be added 
-in the HTML as ng-app = 'myApp'*/
-var myApp = angular.module('myApp', []);
+in the HTML as ng-app = 'myApp'
+We are adding dependencies in the array [ngMessages, ...]*/
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-/*(2) Declaring a controller*/
-myApp.controller('mainController', function(){
+/*(2) Declaring a controller
+We do Dependency injection inside the function($scope, ....)*/
+myApp.controller('mainController', function($scope, $log, $filter){
+
+	/* --Dependency Injection--*/
+	$scope.name = 'Diego';
+	$scope.formattedname = $filter('uppercase')($scope.name);
+
+	$log.info($scope.name);
+	$log.info($scope.formattedname);
+
+	$log.log("Hello.");
+	$log.info("This is some information!");
+	$log.warn("Warning!");
+	$log.debug("Some debugging info!!");
+	$log.error("This was an error!");
 
 });
+
+/* --Dependency Injection and minification--*/
+/*myApp.controller("mainController",["$scope","$log",function(a,b){b.info(a)}]);*/
+	
